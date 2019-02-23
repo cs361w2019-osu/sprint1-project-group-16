2ab415cb9c050683308aa7ae10fcbc66d08a2cdb
Wilsondo <35271544+Wilsondo@users.noreply.github.com>
2019-02-23 14:00:05 -0800

    Unit tests and Sonar up
    It piggybacks upon the result class.

---
+++ src/main/java/cs361/battleships/models/SonarStatus.java
@@ -0,0 +1,20 @@
+package cs361.battleships.models;
+
+public enum SonarStatus {
+
+	/**
+	 * The result if an attack results if there isn't a ship in the square
+	 */
+	EMPTY,
+
+	/**
+	 * The result if an attack results if there is a ship in the square.
+	 */
+	FULL,
+
+	/**
+	 * The result if the coordinates given are invalid.
+	 */
+	INVALID,
+
+}
